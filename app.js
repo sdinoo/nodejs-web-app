d3b3274c4b48bba074986c3b6c51bd39c519e3c6
sdinoo <sdinoo@gmail.com>
2020-05-29 21:40:30 +0530

    updated

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-const http = require('http');
 const k8s = require('@kubernetes/client-node');
-
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Dinesh!\n'
-  res.end(msg);
-});
 
 const kc = new k8s.KubeConfig();
 kc.loadFromCluster();
 
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-k8sApi.listNamespacedPod('api-hub-operator-project')
+
+k8sApi.listNamespacedPod('default')
     .then((res) => {
 	console.log(res.body);
     })
     .catch((err) => {
         console.log(err);
-    });
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+    });	
